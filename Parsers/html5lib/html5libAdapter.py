b759733114017ae0ffaffde4329bf52f521067ed
carlos-anaya <jose.carlos.anaya@gmail.com>
2015-07-22 15:49:03 +0100

    adapter updated to read files as UTF-8

--- Parsers/html5lib/html5libAdapter.py
+++ Parsers/html5lib/html5libAdapter.py
@@ -31,13 +31,14 @@ def convertTreeDump(data):
 #			b.append(line)
 	
 #	return "\n".join(b)
+import codecs
 
 p = html5parser.HTMLParser(tree=html5lib.getTreeBuilder("dom"), namespaceHTMLElements=False)
 
 if len(sys.argv) == 3:
-	if sys.argv[1] == '-f':
-		with open(sys.argv[2], "rb") as f:
-			document = p.parse(f)
+	if sys.argv[1] == '-f':		
+		fp = codecs.open(sys.argv[2], 'rb', 'utf-8')
+		document = p.parse(fp.read())
 	if sys.argv[1] == '-s':
 		document = p.parse(sys.argv[2])
 	if sys.argv[1] == '-u':
